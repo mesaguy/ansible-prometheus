6dcbc097097c0b66ac736968eb84f7f80090e609
Mesaguy <mesaguy@mesaguy.com>
2018-12-10 01:59:52 +0000

    Install sponge where possible without 3rd pary repositories

--- tests/inspec/default/sponge.rb
+++ tests/inspec/default/sponge.rb
@@ -2,8 +2,31 @@
 # author: Mesaguy
 
 # Ensure the 'sponge' command is present
-describe command('which sponge') do
-    its('stdout') { should match /bin/ }
-    its('stderr') { should eq '' }
-    its('exit_status') { should eq 0 }
+
+#describe os.name do
+#  it { should eq 'name' }
+#end
+
+#describe os.family do
+#  it { should eq 'family' }
+#end
+
+#describe os.release do
+#  it { should eq 'release' }
+#end
+
+if os.name == 'alpine'
+    # Unsupported
+elsif os.name == 'amazon' and ['2', '2016.09', '2017.03', '2017.09', '2018.03'].include?(os.release)
+    # Unsupported
+elsif os.family == 'redhat' and os.release.match('^[6-7]\.*')
+    # Unsupported
+elsif os.family == 'suse' and ['13.1', '13.2', '42.1'].include?(os.release)
+    # Unsupported
+else
+    describe command('which sponge') do
+        its('stdout') { should match /bin/ }
+        its('stderr') { should eq '' }
+        its('exit_status') { should eq 0 }
+    end
 end
