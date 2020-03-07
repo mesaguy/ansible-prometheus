49a6f7ddc13e4ee908953ee783d741ad739916ab
Mesaguy <mesaguy@mesaguy.com>
2020-03-07 04:36:45 +0000

    Capabilities are now supported on all supported distros

--- tests/inspec/default/capabilities.rb
+++ tests/inspec/default/capabilities.rb
@@ -3,32 +3,8 @@
 
 # Ensure the 'setcap' command is present
 
-#describe os.name do
-#  it { should eq 'name' }
-#end
-
-#describe os.family do
-#  it { should eq 'family' }
-#end
-
-#describe os.release do
-#  it { should eq 'release' }
-#end
-
-if os.name == 'alpine'
-    # Unsupported
-elsif os.name == 'amazon' and ['2018.03'].include?(os.release)
-    # Unsupported
-#elsif os.name == 'amazon' and ['2', '2016.09', '2017.03', '2017.09', '2018.03'].include?(os.release)
-#    # Unsupported
-#elsif os.family == 'redhat' and os.release.match('^6.*')
-#    # Unsupported
-#elsif os.family == 'suse' and ['13.1', '13.2', '42.1'].include?(os.release)
-#    # Unsupported
-else
-    describe command('PATH=/usr/bin:/usr/sbin:/bin:/sbin which setcap') do
-        its('stdout') { should match /bin/ }
-        its('stderr') { should eq '' }
-        its('exit_status') { should eq 0 }
-    end
+describe command('PATH=/usr/bin:/usr/sbin:/bin:/sbin which setcap') do
+    its('stdout') { should match /bin/ }
+    its('stderr') { should eq '' }
+    its('exit_status') { should eq 0 }
 end
