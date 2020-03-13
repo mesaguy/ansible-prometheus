27a9679afbaa32c552c8f38401784778be0b5949
Mesaguy <mesaguy@mesaguy.com>
2020-03-13 05:40:31 +0000

    Re-adding script tests

---
+++ tests/inspec/default/script_smartmon.sh.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+# author: Mesaguy
+
+describe file('/opt/prometheus/scripts/smartmon.sh') do
+    it { should be_file }
+    it { should be_executable }
+    its('content') { should match /Script informed by the collectd monitoring script for smartmontools/ }
+    its('size') { should > 6500 }
+    its('mode') { should cmp '0555' }
+    its('owner') { should eq 'root' }
+    its('group') { should eq 'prometheus' }
+end
