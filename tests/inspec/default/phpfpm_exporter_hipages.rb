37ab88f218224704da83990ddc0903f65dc61c56
Mesaguy <mesaguy@mesaguy.com>
2020-04-18 06:01:46 +0000

    Correct author's name

--- tests/inspec/default/phpfpm_exporter_hipages.rb
+++ tests/inspec/default/phpfpm_exporter_hipages.rb
@@ -1,14 +1,14 @@
 # encoding: utf-8
 # author: Mesaguy
 
-describe file('/opt/prometheus/exporters/phpfpm_exporter_perconalab/active') do
+describe file('/opt/prometheus/exporters/phpfpm_exporter_hipages/active') do
     it { should be_symlink }
     its('mode') { should cmp '0755' }
     its('owner') { should eq 'root' }
     its('group') { should eq 'prometheus' }
 end
 
-describe file('/opt/prometheus/exporters/phpfpm_exporter_perconalab/active/php-fpm_exporter') do
+describe file('/opt/prometheus/exporters/phpfpm_exporter_hipages/active/php-fpm_exporter') do
     it { should be_file }
     it { should be_executable }
     its('mode') { should cmp '0755' }
@@ -16,13 +16,13 @@ describe file('/opt/prometheus/exporters/phpfpm_exporter_perconalab/active/php-f
     its('group') { should eq 'prometheus' }
 end
 
-describe service('phpfpm_exporter_perconalab') do
+describe service('phpfpm_exporter_hipages') do
     it { should be_enabled }
     it { should be_installed }
     it { should be_running }
 end
 
-describe processes(Regexp.new("^/opt/prometheus/exporters/phpfpm_exporter_perconalab/(v)?([0-9.]+|[0-9.]+__go-[0-9.]+)/php-fpm_exporter")) do
+describe processes(Regexp.new("^/opt/prometheus/exporters/phpfpm_exporter_hipages/(v)?([0-9.]+|[0-9.]+__go-[0-9.]+)/php-fpm_exporter")) do
     it { should exist }
     its('entries.length') { should eq 1 }
     its('users') { should include 'prometheus' }
