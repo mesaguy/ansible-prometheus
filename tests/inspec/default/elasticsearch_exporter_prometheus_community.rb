a01c2da93c554f96e3fef46164e33f8c00dc9a87
Mesaguy <mesaguy@mesaguy.com>
2021-06-07 04:22:01 +0000

    The elasticsearch_exporter name changed from elasticsearch_exporter_justwatchcom to elasticsearch_exporter_prometheus_community

--- tests/inspec/default/elasticsearch_exporter_prometheus_community.rb
+++ tests/inspec/default/elasticsearch_exporter_prometheus_community.rb
@@ -1,14 +1,14 @@
 # encoding: utf-8
 # author: Mesaguy
 
-describe file('/opt/prometheus/exporters/elasticsearch_exporter_justwatchcom/active') do
+describe file('/opt/prometheus/exporters/elasticsearch_exporter_prometheus_community/active') do
     it { should be_symlink }
     its('mode') { should cmp '0755' }
     its('owner') { should eq 'root' }
     its('group') { should eq 'prometheus' }
 end
 
-describe file('/opt/prometheus/exporters/elasticsearch_exporter_justwatchcom/active/elasticsearch_exporter') do
+describe file('/opt/prometheus/exporters/elasticsearch_exporter_prometheus_community/active/elasticsearch_exporter') do
     it { should be_file }
     it { should be_executable }
     its('mode') { should cmp '0755' }
@@ -16,13 +16,13 @@ describe file('/opt/prometheus/exporters/elasticsearch_exporter_justwatchcom/act
     its('group') { should eq 'prometheus' }
 end
 
-describe service('elasticsearch_exporter_justwatchcom') do
+describe service('elasticsearch_exporter_prometheus_community') do
     it { should be_enabled }
     it { should be_installed }
     it { should be_running }
 end
 
-describe processes(Regexp.new("^/opt/prometheus/exporters/elasticsearch_exporter_justwatchcom/(v)?([0-9.]+|[0-9.]+__go-[0-9.]+)/elasticsearch_exporter")) do
+describe processes(Regexp.new("^/opt/prometheus/exporters/elasticsearch_exporter_prometheus_community/(v)?([0-9.]+|[0-9.]+__go-[0-9.]+)/elasticsearch_exporter")) do
     it { should exist }
     its('entries.length') { should eq 1 }
     its('users') { should include 'prometheus' }
