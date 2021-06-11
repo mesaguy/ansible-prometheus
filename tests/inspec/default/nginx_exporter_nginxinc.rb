e12167c0a5db1a2eda04a4882e1148eccc283adf
Mesaguy <mesaguy@mesaguy.com>
2021-06-11 03:42:36 +0000

    Complete changes started in #33

--- tests/inspec/default/nginx_exporter_nginxinc.rb
+++ tests/inspec/default/nginx_exporter_nginxinc.rb
@@ -1,14 +1,14 @@
 # encoding: utf-8
 # author: Mesaguy
 
-describe file('/opt/prometheus/exporters/nginx-prometheus-exporter_nginxinc/active') do
+describe file('/opt/prometheus/exporters/nginx_exporter_nginxinc/active') do
     it { should be_symlink }
     its('mode') { should cmp '0755' }
     its('owner') { should eq 'root' }
     its('group') { should eq 'prometheus' }
 end
 
-describe file('/opt/prometheus/exporters/nginx-prometheus-exporter_nginxinc/active/nginx-prometheus-exporter') do
+describe file('/opt/prometheus/exporters/nginx_exporter_nginxinc/active/nginx-prometheus-exporter') do
     it { should be_file }
     it { should be_executable }
     its('mode') { should cmp '0755' }
@@ -16,7 +16,7 @@ describe file('/opt/prometheus/exporters/nginx-prometheus-exporter_nginxinc/acti
     its('group') { should eq 'prometheus' }
 end
 
-describe service('nginx-prometheus-exporter_nginxinc') do
+describe service('nginx_exporter_nginxinc') do
     it { should be_enabled }
     it { should be_installed }
 end
